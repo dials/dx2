5e8390b4e811f01fd0ae78b7ae52cfdd3339e732
James Beilsten-Edmands <30625594+jbeilstenedmands@users.noreply.github.com>
2025-08-07 13:25:36 +0100

    Add simple panel constructor

--- include/dx2/detector.hpp
+++ include/dx2/detector.hpp
@@ -3,6 +3,7 @@
 #include <Eigen/Dense>
 #include <math.h>
 #include <nlohmann/json.hpp>
+#include <set>
 
 using json = nlohmann::json;
 using Eigen::Matrix3d;
@@ -69,6 +70,12 @@ class Panel {
 public:
   Panel() = default;
   Panel(json panel_data);
+  Panel(double distance,
+    std::array<double, 2> beam_center,
+    std::array<double, 2> pixel_size,
+    std::array<int, 2> image_size,
+    const std::string& fast_axis="x",
+    const std::string& slow_axis="-y"); // Simple constructor for flat panel perpendicular to beam.
   Matrix3d get_d_matrix() const;
   std::array<double, 2> px_to_mm(double x, double y) const;
   std::array<double, 2> mm_to_px(double x, double y) const;
@@ -82,6 +89,7 @@ public:
   std::array<double, 2> get_image_size_mm() const;
   double get_directed_distance() const;
   void update(Matrix3d d);
+  void set_correction_parameters(double thickness, double mu, bool parallax_correction);
 
 protected:
   // panel_frame items
@@ -125,6 +133,54 @@ void Panel::update(Matrix3d d) {
   slow_axis_ = {d(0, 1), d(1, 1), d(2, 1)};
   origin_ = {d(0, 2), d(1, 2), d(2, 2)};
   normal_ = fast_axis_.cross(slow_axis_);
+}
+
+const std::set<std::string> valid_axes = {"x", "-x", "y", "-y"};
+const std::map<std::string, Vector3d> axis_map = {
+    {"x",  Vector3d(1.0,  0.0, 0.0)},
+    {"-x",  Vector3d(-1.0,  0.0, 0.0)},
+    {"y",  Vector3d(0.0,  1.0, 0.0)},
+    {"-y",  Vector3d(0.0,  -1.0, 0.0)}
+};
+
+Panel::Panel(
+  double distance,
+  std::array<double, 2> beam_center,
+  std::array<double, 2> pixel_size,
+  std::array<int, 2> image_size,
+  const std::string& fast_axis,
+  const std::string& slow_axis
+) : pixel_size_(pixel_size), image_size_(image_size){
+  if (valid_axes.find(fast_axis) == valid_axes.end()) {
+      throw std::invalid_argument("Invalid fast_axis: " + fast_axis);
+  }
+  if (valid_axes.find(slow_axis) == valid_axes.end()) {
+      throw std::invalid_argument("Invalid fast_axis: " + slow_axis);
+  }
+  origin_ = {0.,0.,-1.0*distance};
+  fast_axis_ = axis_map.find(fast_axis)->second;
+  slow_axis_ = axis_map.find(slow_axis)->second;
+  origin_ -= beam_center[0] * pixel_size_[0] * fast_axis_;
+  origin_ -= beam_center[1] * pixel_size_[1] * slow_axis_;
+  normal_ = fast_axis_.cross(slow_axis_);
+  Matrix3d d_matrix{{fast_axis_[0], slow_axis_[0], origin_[0]},
+                    {fast_axis_[1], slow_axis_[1], origin_[1]},
+                    {fast_axis_[2], slow_axis_[2], origin_[2]}};
+  d_ = d_matrix;
+  D_ = d_.inverse();
+}
+
+void Panel::set_correction_parameters(double thickness, double mu, bool parallax_correction=true){
+  thickness_ = thickness;
+  mu_ = mu;
+  if (parallax_correction){
+    parallax_correction_ = true;
+    pixel_to_mm_strategy_ = "ParallaxCorrectedPxMmStrategy";
+  }
+  else {
+    parallax_correction_ = false;
+    pixel_to_mm_strategy_ = "SimplePxMmStrategy";
+  }
 }
 
 Panel::Panel(json panel_data) {
