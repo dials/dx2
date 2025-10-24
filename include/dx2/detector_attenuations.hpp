bb7c09e7dcb454f94b9c4fb1f95f65e4e1ff63b5
James Beilsten-Edmands <30625594+jbeilstenedmands@users.noreply.github.com>
2025-10-24 12:01:29 +0100

    Add test

--- include/dx2/detector_attenuations.hpp
+++ include/dx2/detector_attenuations.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <string>
 
 double calculate_mu_for_material_at_wavelength(const std::string &material,
                                                double wavelength);
---
+++ tests/test_detector_attenuations.cxx
@@ -0,0 +1,14 @@
+#include <dx2/detector_attenuations.hpp>
+#include <gtest/gtest.h>
+#include <string>
+
+TEST(ModelTests, CrystalTest) {
+    std::string silicon = "Si";
+    std::string cdte = "CdTe";
+    double mu_si = calculate_mu_for_material_at_wavelength(silicon, 0.976254);
+    EXPECT_NEAR(mu_si, 3.9220836, 1E-6);
+
+    double mu_cdte = calculate_mu_for_material_at_wavelength(cdte, 0.4959);
+    EXPECT_NEAR(mu_cdte, 7.2858499, 1E-6);
+
+}
